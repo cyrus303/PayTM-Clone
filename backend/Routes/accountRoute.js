a67f471678b99a605b320e9aed833f6d44709866
Sachin <sachinsunny303@gmail.com>
2024-01-30 17:46:37 +0000

    structured backend into MVC architecture

--- backend/Routes/accountRoute.js
+++ backend/Routes/accountRoute.js
@@ -1,73 +1,18 @@
 const express = require('express');
-const authenticateUser = require('../Middleware/authenticateUser');
-const accountModel = require('../Model/accountModel');
 const router = express.Router();
-const z = require('zod');
+const authenticateUser = require('../Middleware/authenticateUser');
+const accountController = require('../Controllers/accountController');
 
-router.get('/balance', authenticateUser, async (req, res) => {
-  try {
-    const {userId} = req;
-    const UserBalance = await accountModel.findOne({userId});
-    res.status(200).send({
-      balance: UserBalance.balance,
-    });
-  } catch (error) {
-    console.error('Error reading user balance', error);
-    res.status(500).send({error: 'Internal Server Error'});
-  }
-});
+router.get(
+  '/balance',
+  authenticateUser,
+  accountController.balanceCheck
+);
 
-router.post('/transfer', authenticateUser, async (req, res) => {
-  const transferSchema = z.object({
-    to: z.string(),
-    amount: z.number().gte(1),
-  });
-
-  const {success} = transferSchema.safeParse(req.body);
-  if (!success) {
-    return res.status(411).json({
-      message: 'Incorrect input formats',
-    });
-  }
-
-  try {
-    const {to, amount} = req.body;
-    const {userId} = req;
-
-    let CurrentUser = await accountModel.findOne({userId: userId});
-
-    if (CurrentUser.balance < amount) {
-      return res.status(400).send({
-        message: 'Insufficient funds',
-      });
-    }
-
-    const operations = [
-      {
-        updateOne: {
-          filter: {userId: to},
-          update: {$inc: {balance: amount}},
-          new: true,
-        },
-      },
-      {
-        updateOne: {
-          filter: {userId},
-          update: {$inc: {balance: -amount}},
-          new: true,
-        },
-      },
-    ];
-
-    const result = await accountModel.bulkWrite(operations);
-    res.status(200).send({message: 'Transfer successful'});
-  } catch (error) {
-    console.log('catch');
-    console.log(error);
-    return res.status(400).json({
-      message: 'Invalid account',
-    });
-  }
-});
+router.post(
+  '/transfer',
+  authenticateUser,
+  accountController.transferAmount
+);
 
 module.exports = router;
